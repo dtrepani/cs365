ab90d2753da772f9924f79500ae5f4ea309ac857
Fina <finapani@gmail.com>
2016-02-11 15:47:10 -0500

    InClass02: Finished.

--- InClass/InClass01/script.js
+++ InClass/InClass01/script.js
@@ -1,66 +1,121 @@
 "use strict";
 
-var	companionSelected = false;
-
-var Companion = function(aName, aEats, aButtonName) {
+var Companion = function(aName, aEats) {
 	this.name = aName;
 	this.eats = aEats;
 	this.leftSideOfRiver = true;
-	this.buttonName = aButtonName;
+	this.buttonName = 'input[type="button"][value="' + aName + '"]';
 };
 
 Companion.prototype.writeHTML = function() {
 	return "<input type=\"button\" value=\"" + this.name + "\"" + ((this.name == 'You') ? "class=\"disabled\"" : "") + ">";
 };
 
-var	you		= new Companion('You', null, 'input[type="button"][value="You"]'),
-	wolf	= new Companion('Wolf', 'Goat', 'input[type="button"][value="Wolf"]'),
-	goat	= new Companion('Goat', 'Cabbage', 'input[type="button"][value="Goat"]'),
-	cabbage	= new Companion('Cabbage', null, 'input[type="button"][value="Cabbage"]');
+Companion.prototype.isOnSameSideAs = function(companion) {
+	return (this.leftSideOfRiver == companion.leftSideOfRiver);
+};
 
+var	you		= new Companion('You', null),
+	wolf	= new Companion('Wolf', 'Goat'),
+	goat	= new Companion('Goat', 'Cabbage'),
+	cabbage	= new Companion('Cabbage', null);
 var companions = [wolf, goat, cabbage];
+var	companionSelected = false;
 
 var updateCompanions = function() {
-	var	i,
-		containerLeft = '',
-		containerRight = '';
-	you.leftSideOfRiver = !you.leftSideOfRiver;
+	var	i;
 
-	if(you.leftSideOfRiver) {
-		containerLeft += you.writeHTML();
-	} else {
-		containerRight += you.writeHTML();
-	}
-
+	checkAndMoveToProperSideOfRiver(you);
 	for (i = 0; i < companions.length; i++) {
-		if($(companions[i].buttonName).hasClass('disabled')) {
-			companions[i].leftSideOfRiver = !companions[i].leftSideOfRiver;
-		}
-
-		if(companions[i].leftSideOfRiver) {
-			containerLeft += companions[i].writeHTML();
-		} else {
-			containerRight += companions[i].writeHTML();
-		}
+		checkAndMoveToProperSideOfRiver(companions[i]);
 	}
 
-	$('#left-side').html(containerLeft);
-	$('#right-side').html(containerRight);
-
+	companionSelected = false;
 	for (i = 0; i < companions.length; i++) {
 		$(companions[i].buttonName).removeClass('disabled');
 	}
 
-	companionSelected = false;
+	checkForGameOver();
+	checkForWin();
 };
 
+var reset = function() {
+	you.leftSideOfRiver = false;
+	checkAndMoveToProperSideOfRiver(you);
+	for(var i = 0; i < companions.length; i++) {
+		companions[i].leftSideOfRiver = true;
+		checkAndMoveToProperSideOfRiver(companions[i]);
+	}
+
+	companionSelected = false;
+
+	$('.overlay').removeClass('is-visible');
+	$('#win').removeClass('is-visible');
+	$('#game-over p').remove();
+	$('#game-over').removeClass('is-visible');
+};
+
+function checkAndMoveToProperSideOfRiver(companion) {
+	if($(companion.buttonName).hasClass('disabled')) companion.leftSideOfRiver = !companion.leftSideOfRiver;
+	$(companion.buttonName).appendTo((companion.leftSideOfRiver) ? $('#left-side') : $('#right-side'));
+}
+
+function getCompanionIndex(companionName) {
+	for(var i = 0; i < companions.length; i++) {
+		if(companions[i].name == companionName) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+function checkForGameOver() {
+	var gameOver = false;
+	var deathMsg = $('<p />');
+
+	for(var i = 0; i < companions.length; i++) {
+		if(!companions[i].isOnSameSideAs(you)) {
+			var indexOfPrey = getCompanionIndex(companions[i].eats);
+			if((indexOfPrey != -1) && companions[i].isOnSameSideAs(companions[indexOfPrey])) {
+				deathMsg.append("The " + companions[i].name + " ate the " + companions[i].eats + "!");
+				gameOver = true;
+				break;
+			}
+		}
+	}
+
+	if(gameOver) {
+		$('.overlay').addClass('is-visible');
+		$('#game-over h1').after(deathMsg);
+		$('#game-over').addClass('is-visible');
+	}
+}
+
+function checkForWin() {
+	var win = true;
+
+	if(you.leftSideOfRiver) win = false;
+	for(var i = 0; i < companions.length; i++) {
+		if(companions[i].leftSideOfRiver) {
+			win = false;
+			break;
+		}
+	}
+
+	if(win) {
+		$('.overlay').addClass('is-visible');
+		$('#win').addClass('is-visible');
+	}
+}
+
 function buttonClick(event) {
-	var button = $(event.data.param);
+	var	companion = event.data.param,
+		button = $(companion.buttonName);
 
 	if(button.hasClass('disabled')) {
 		companionSelected = false;
 		button.removeClass('disabled');
-	} else if(!companionSelected) {
+	} else if(!companionSelected && (companion.isOnSameSideAs(you))) {
 		companionSelected = true;
 		button.addClass('disabled');
 	}
@@ -68,10 +123,11 @@ function buttonClick(event) {
 
 function initClicks() {
 	for (var i = 0; i < companions.length; i++) {
-		$(companions[i].buttonName).click({ param: companions[i].buttonName }, buttonClick);
+		$(companions[i].buttonName).click({ param: companions[i] }, buttonClick);
 	}
-
 	$('#go').click(updateCompanions);
+
+	$('input[type="button"][value="Reset?"]').click(reset);
 }
 
 $(function() {
